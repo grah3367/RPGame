4dd296b1d1a8726cca2bb53743cbdb164b6d2879
grah3367 <grah3367@chawk.cecil.edu>
2016-04-24 20:18:15 -0400

    file structure

---
+++ src/RPG-Game/ActiveSession.java
@@ -0,0 +1,15 @@
+/*
+ * @author Ian Gompers
+ * @date created Fri, Apr 15, 2016  2:36:34 AM
+ * @last modified Fri, Apr 15, 2016  2:57:33 AM
+ * @description This class holds the active player character for an initiated session.
+ */
+public class ActiveSession {
+
+    // LECACY - REPLACED BY HANDLER
+
+    // object data
+    Character activePlayer = new Character(0, 0, 0, 0, 0);
+    Character activeEnemy = new Character(0, 0, 0, 0, 0);
+
+} // end ActiveSession class
